0af12bb71f97413c84c893d167083ba6b84f103a
Courtney Burns <courtneylynnburns@gmail.com>
2016-03-23 13:33:51 -0500

    all tables added

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :users
+    resources :award_categories
+    resources :ballots
+    resources :nominations
     resources :plays
     resources :theaters
+    resources :viewings
+    resources :votes
 
     root to: "users#index"
   end
