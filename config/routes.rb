130dbef59d817afad173c854bad8bcdebd8af136
Courtney Burns <courtneylynnburns@gmail.com>
2016-03-23 09:30:25 -0500

    still not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    resources :users
+resources :plays
+resources :theaters
+
+    root to: "users#index"
+  end
+
   devise_for :users
   namespace :admin do
     resources :theaters
