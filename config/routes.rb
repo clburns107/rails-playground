4cb0ec668df67382c5f6e6d736075835eb7c5168
Courtney Burns <courtneylynnburns@gmail.com>
2016-03-22 22:10:20 -0500

    devise and administrate installed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  namespace :admin do
+    resources :theaters
+
+    root to: "theaters#index"
+  end
+
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,7 @@
 FactoryGirl.define do
+  factory :user do
+    
+  end
   factory :theater do
     name "MyString"
   end
