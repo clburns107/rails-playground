ccc23a46c47c203b56ca63fe29d97db66000048c
Courtney Burns <courtneylynnburns@gmail.com>
2016-03-23 13:55:26 -0500

    fixed dashboard for theaters

--- app/dashboards/theater_dashboard.rb
+++ app/dashboards/theater_dashboard.rb
@@ -8,7 +8,6 @@ class TheaterDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
-    plays: Field::HasMany,
     id: Field::Number,
     name: Field::String,
     created_at: Field::DateTime,
@@ -21,7 +20,6 @@ class TheaterDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :plays,
     :id,
     :name,
     :created_at,
@@ -30,7 +28,6 @@ class TheaterDashboard < Administrate::BaseDashboard
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
-    :plays,
     :id,
     :name,
     :created_at,
@@ -41,7 +38,6 @@ class TheaterDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
-    :plays,
     :name,
   ]
 
