ab05f7302f989feeb6f5fce09ba37a076152651a
Courtney Burns <courtneylynnburns@gmail.com>
2016-03-23 10:35:42 -0500

    dashboards work now

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,15 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :users
-resources :plays
-resources :theaters
+    resources :plays
+    resources :theaters
 
     root to: "users#index"
   end
 
   devise_for :users
-  namespace :admin do
-    resources :theaters
-
-    root to: "theaters#index"
-  end
-
+  
 end
+
+
+
